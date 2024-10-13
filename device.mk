10cb0ade06ffb6fec28e07e566d28251adac4494
HarjeetSingh <harjeet.singh5101@gmail.com>
2024-10-13 22:20:56 +0530

    inherit viper

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/MiuiCamera.mk)
 
+#Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
